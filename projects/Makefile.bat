f1792bd76d4ff834db2693efa096cf74b6d85058
Jeff Nyman <jeffnyman@gmail.com>
2019-07-31 04:47:06 -0500

    Added clean to Windows makefile.

--- projects/Makefile.bat
+++ projects/Makefile.bat
@@ -2,6 +2,7 @@
 
 if /I %1 == build goto :build
 if /I %1 == play goto :play
+if /I %1 == clean goto :clean
 
 :build
 ..\bin\zilf.exe %2.zil
@@ -10,4 +11,9 @@ goto :eof
 
 :play
 %2.z3
+goto :eof
+
+:clean
+del *.zap /f /q
+del *.z? /f /q
 goto :eof
